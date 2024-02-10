b19b74f66fdd00db0a552994b186e0ef95e75131
Denis Nepriakhin <arunaweb@users.noreply.github.com>
2024-02-10 12:58:10 +0100

    Updated data of persons.

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -13,42 +13,42 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         "id": 1,
         "email": "george.bluth@reqres.in",
-        "first_name": "George",
+        "first_name": "George Loc-Db",
         "last_name": "Bluth",
         "avatar": "https://reqres.in/img/faces/1-image.jpg"
       },
       {
           "id": 2,
           "email": "janet.weaver@reqres.in",
-          "first_name": "Janet",
+          "first_name": "Janet Loc-Db",
           "last_name": "Weaver",
           "avatar": "https://reqres.in/img/faces/2-image.jpg"
       },
       {
           "id": 3,
           "email": "emma.wong@reqres.in",
-          "first_name": "Emma",
+          "first_name": "Emma Loc-Db",
           "last_name": "Wong",
           "avatar": "https://reqres.in/img/faces/3-image.jpg"
       },
       {
           "id": 4,
           "email": "eve.holt@reqres.in",
-          "first_name": "Eve",
+          "first_name": "Eve Loc-Db",
           "last_name": "Holt",
           "avatar": "https://reqres.in/img/faces/4-image.jpg"
       },
       {
           "id": 5,
           "email": "charles.morris@reqres.in",
-          "first_name": "Charles",
+          "first_name": "Charles Loc-Db",
           "last_name": "Morris",
           "avatar": "https://reqres.in/img/faces/5-image.jpg"
       },
       {
           "id": 6,
           "email": "tracey.ramos@reqres.in",
-          "first_name": "Tracey",
+          "first_name": "Tracey Loc-Db",
           "last_name": "Ramos",
           "avatar": "https://reqres.in/img/faces/6-image.jpg"
       }
